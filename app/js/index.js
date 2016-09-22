3e2635e596e3605a12db50ab9cd61e638734786b
Melanie Burger <melanie.brgr@gmail.com>
2016-09-21 21:26:17 -0400

    Initial scripts

---
+++ app/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    // console.log("ready");
+    let $hexConnect = $(".col-1:contains(connect)");
+    $hexConnect.click(() => {
+    	console.log('you clicked connect!');
+    });
+});
